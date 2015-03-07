921f3df98f66037caa53ada0acb8605ffb276bfe
Petre Pătrașc <petre@dreamlabs.ro>
2015-03-07 23:06:55 +0200

    ZEN-1: Add Behat feature for Question Category create

---
+++ src/ApiBundle/Features/Context/FeatureContext.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ApiBundle\Features\Context;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Defines application features from the specific context.
+ *
+ * @package ApiBundle\Features\Context
+ * @author  Petre Pătrașc <petre@dreamlabs.ro>
+ */
+class FeatureContext extends BaseApiFeature implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct($host)
+    {
+        parent::__construct($host);
+    }
+
+    /**
+     * @Given I want to make an API request with the data in file :arg1
+     */
+    public function iWantToMakeAnApiRequestWithTheDataInFile($file)
+    {
+        $fileContent = $this->readFile($file);
+        $this->setRequestData($fileContent);
+    }
+
+    /**
+     * @When I make a :arg1 request to :arg2
+     */
+    public function iAmMakingARequestTo($requestMethod, $requestUrl)
+    {
+        $requestData     = $this->getRequestData();
+        $responseWrapper = $this->getGuzzleClient()->createRequest($requestMethod, $requestUrl, null, $requestData)->send();
+        $this->setResponseData($responseWrapper);
+    }
+
+    /**
+     * @Then the response has a status code of :arg1
+     */
+    public function theResponseHasAStatusCodeOf($expectedStatusCode)
+    {
+        $actualStatusCode = $this->getResponseData()->getStatusCode();
+
+        \PHPUnit_Framework_TestCase::assertEquals($expectedStatusCode, $actualStatusCode, "Expected the response to have a status code of {$actualStatusCode}.");
+    }
+
+    /**
+     * @Then the response contains valid JSON
+     */
+    public function theResponseContainsValidJson()
+    {
+        $responseBody = $this->getResponseData()->getBody();
+
+        \PHPUnit_Framework_TestCase::assertJson($responseBody, 'Expected the response to be valid JSON.');
+    }
+
+    /**
+     * @Then the response has the same contents as the file :arg1
+     */
+    public function theResponseHasTheSameContentsAsTheFile($file)
+    {
+        $actualContent   = $this->getResponseData()->getBody();
+        $expectedContent = $this->readFile($file);
+
+        \PHPUnit_Framework_TestCase::assertJsonStringEqualsJsonString($expectedContent, $actualContent, "Expected the response to match the one in file {$file}.");
+    }
+
+
+}
--- src/ApiBundle/Features/question_category.feature
+++ src/ApiBundle/Features/question_category.feature
@@ -4,6 +4,8 @@ Feature: Question Category CRUD
   In order to be able to group questions into specific Categories that can be included into Tests
 
   Scenario: Create Question Category
-    Given I am making a POST request to "/api/question-category/"
-    When I send the data in file "api-mocks/question_category.create.json"
-    Then I should get the API response in "api-mocks/question_category.create.json"
+    Given I want to make an API request with the data in file "/api-mock/question_category.create.json"
+    When I make a "POST" request to "/api/question-category/"
+    Then the response has a status code of "200"
+    And the response contains valid JSON
+    And the response has the same contents as the file "api-mocks/question_category.create.json"
