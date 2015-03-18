ebad720f073e84379cd471d7e8688f6269b1fe12
Petre Pătrașc <petre@dreamlabs.ro>
2015-03-18 02:50:46 +0200

    ZEN-3: Add Question entity and link to Question Category

---
+++ src/Zenium/AppBundle/Repository/QuestionRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Zenium\AppBundle\Repository;
+
+/**
+ * Repository for the Question Entity.
+ *
+ * @package AppBundle\Repository
+ * @author  Petre Pătrașc <petre@dreamlabs.ro>
+ */
+class QuestionRepository extends AbstractBaseRepository
+{
+
+}
